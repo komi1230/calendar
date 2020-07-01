30644a0ff35a8a3e11e195429ca3f3171c59c91d
Yusuke Kominami <yukke.konan@gmail.com>
2020-07-01 22:04:18 +0900

    created top page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { CreatePage } from './pages/Top';
+import './styles/App.css';
 
-function App() {
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <CreatePage />
+  )
 }
 
 export default App;
--- frontend/src/pages/Top.tsx
+++ frontend/src/pages/Top.tsx
@@ -1,7 +1,64 @@
 import React from 'react';
+import { Grid, TextField } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import { SearchAppBar } from './Header';
+
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    mainMessage: {
+      flexGrow: 1,
+      alignItems: 'center',
+      fontSize: '1.2rem',
+      marginTop: theme.spacing(15),
+      [theme.breakpoints.up('sm')]: {
+        marginTop: theme.spacing(20),
+        width: 'auto',
+        fontSize: '2rem',
+      },
+    },
+    searchField: {
+      textAlign: "center",
+      marginTop: theme.spacing(5),
+      [theme.breakpoints.up('sm')]: {
+        marginTop: theme.spacing(10),
+        width: 'auto',
+      },
+    },
+  })
+);
 
 export const CreatePage: React.FC = () => {
-    return (
-        <>hoge</>
-    )
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <SearchAppBar />
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >
+        <Grid
+          item
+        >
+          <Typography
+            className={classes.mainMessage}
+          >
+            Search someone's calendar
+          </Typography>
+        </Grid>
+        <TextField
+          id="outlined-basic"
+          label=""
+          placeholder="ID"
+          className={classes.searchField}
+        />
+      </Grid>
+    </div>
+  )
 }
