8b0046c06ba620223ea34957cae3940b2dbdd83b
Yusuke Kominami <yukke.konan@gmail.com>
2020-07-07 23:17:47 +0900

    view Calendar Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { CreatePage } from './pages/Top';
 import { Table } from './pages/Table';
-import './styles/App.css';
 
 
 const App: React.FC = () => {
   return (
-    <CreatePage />
-    //<Table />
+    //<CreatePage />
+    <Table />
   )
 }
 
