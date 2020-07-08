00da244e2b8d56fe9857de1e790a6abcc962112d
Yusuke Kominami <yukke.konan@gmail.com>
2020-07-08 22:46:01 +0900

    initialized with actix-web

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello World !")
+}
+
+async fn index2() -> impl Responder {
+    HttpResponse::Ok().body("Goodbye !")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/bye", web::get().to(index2))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
