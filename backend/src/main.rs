015a565171fac2853490458e3549364b943d5777
Yusuke Kominami <yukke.konan@gmail.com>
2020-07-09 22:04:01 +0900

    tried systemfd

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,21 +1,20 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use listenfd::ListenFd;
 
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello World !")
-}
-
-async fn index2() -> impl Responder {
-    HttpResponse::Ok().body("Goodbye !")
+async fn index(_: HttpRequest) -> impl Responder {
+    "hogehgehoge"
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(index))
-            .route("/bye", web::get().to(index2))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    let mut listenfd = ListenFd::from_env();
+    let mut server = HttpServer::new(|| App::new().route("/", web::get().to(index)));
+
+    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
+        server.listen(l)?
+    } else {
+        server.bind("127.0.0.1:3000")?
+    };
+
+    server.run().await
 }
