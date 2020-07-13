0130877f79c1f8dee4be96d421f797f04b0c5ea7
Yusuke Kominami <yukke.konan@gmail.com>
2020-07-14 00:02:37 +0900

    initialized database setting

---
+++ backend/src/schema.rs
@@ -0,0 +1,20 @@
+table! {
+    schedule (id) {
+        id -> Nullable<Text>,
+        username -> Text,
+        fromtime -> Nullable<Timestamp>,
+        totime -> Nullable<Timestamp>,
+    }
+}
+
+table! {
+    users (username) {
+        username -> Text,
+        registerdate -> Nullable<Timestamp>,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(
+    schedule,
+    users,
+);
