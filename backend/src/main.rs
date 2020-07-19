1fa4b149c9063f2e625af9e395f18dca4318f071
Yusuke Kominami <yukke.konan@gmail.com>
2020-07-19 21:36:26 +0900

    confirmed projects works fine

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,6 +5,7 @@ use calendar::views::{create_user, schedule_content, search_user};
 
 #[get("/hello")]
 async fn hello() -> impl Responder {
+    println!("Got Access !!");
     HttpResponse::Ok().body("Hey there!")
 }
 
@@ -13,15 +14,13 @@ async fn main() -> std::io::Result<()> {
     let pool = make_pool();
 
     let app = move || {
-        App::new()
-            .service(
-                web::scope("/")
-                    .data(pool.clone())
-                    .service(create_user)
-                    .service(search_user)
-                    .service(schedule_content),
-            )
-            .service(hello)
+        App::new().service(hello).service(
+            web::scope("/")
+                .data(pool.clone())
+                .service(create_user)
+                .service(search_user)
+                .service(schedule_content),
+        )
     };
 
     HttpServer::new(app).bind("127.0.0.1:8000")?.run().await
