ff3d3d614b04782785061009b7c7c868e1a599b1
Yusuke Kominami <yukke.konan@gmail.com>
2020-07-19 16:21:09 +0900

    fixed small bugs

--- backend/migrations/2020-07-13-143441_calendar/up.sql
+++ backend/migrations/2020-07-13-143441_calendar/up.sql
@@ -3,15 +3,15 @@ CREATE TABLE users (
     registerdate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO users (username) VALUES ("tmp_name");
+INSERT INTO users (username) VALUES ('tmp_name');
 
 
 CREATE TABLE schedules (
-    id VARCHAR PRIMARY KEY,
+    id SERIAL NOT NULL  PRIMARY KEY,
     username VARCHAR,
     fromtime TIMESTAMP,
     totime TIMESTAMP
-)
+);
 
---INSERT INTO schedules (username, fromtime, totime) VALUES ("komi", "2020-07-18 00:00:00", "2020-07-20 00:00:00");
---INSERT INTO schedules (username, fromtime, totime) VALUES ("komi", "2020-07-21 08:00:00", "2020-07-21 18:00:00");
+INSERT INTO schedules (username, fromtime, totime) VALUES ('komi', '2020-07-18 00:00:00', '2020-07-20 00:00:00');
+INSERT INTO schedules (username, fromtime, totime) VALUES ('komi', '2020-07-21 08:00:00', '2020-07-21 18:00:00');
