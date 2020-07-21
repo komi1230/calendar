1f54beeac4d7252f725d52c5d783ac5b9099f3b4
Yusuke Kominami <yukke.konan@gmail.com>
2020-07-21 20:38:59 +0900

    enabled to select month

--- frontend/src/pages/Table.tsx
+++ frontend/src/pages/Table.tsx
@@ -6,6 +6,7 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Collapse from '@material-ui/core/Collapse';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import { SearchAppBar } from './Header';
@@ -21,7 +22,7 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     tile: {
       minWidth: '40px',
-      maxHeight: '50px', 
+      maxHeight: '50px',
       borderRadius: '0',
       [theme.breakpoints.up('sm')]: {
         minWidth: '80px',
@@ -39,37 +40,6 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-interface CalendarPageProps {
-  year: number,
-  month: number,
-};
-
-const MonthHeader: React.FC<CalendarPageProps> = (props) => {
-  const classes = useStyles();
-  const year = props.year;
-  const month = props.month;
-  return (
-    <Box bgcolor="text.secondary" color="background.paper" p={2} className={classes.month}>
-      <Grid
-        container
-        direction="row"
-        justify="center"
-        alignItems="center"
-      >
-        <Grid item>
-          <ChevronLeftIcon />
-        </Grid>
-        <Grid item xs={6} sm={2}>
-          {year} / {month}
-        </Grid>
-        <Grid item>
-          <ChevronRightIcon />
-        </Grid>
-      </Grid>
-    </Box>
-  )
-};
-
 interface TileProps {
   year: number,
   month: number,
@@ -82,13 +52,13 @@ const Tile: React.FC<TileProps> = (props) => {
   const onClick = props.onClick;
   const classes = useStyles();
   return (
-    <Button 
+    <Button
       //variant="outlined" 
-      onClick={onClick} 
-      size="small" 
+      onClick={onClick}
+      size="small"
       className={classes.tile}
     >
-      <Grid 
+      <Grid
         container
         direction="column"
         justify="center"
@@ -118,7 +88,7 @@ interface WeekTilesProps {
 const WeekTiles: React.FC<WeekTilesProps> = (props) => {
   const weekDays: string[] = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
-  const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth()+1);
+  const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth() + 1);
   const [selectedDate, setSelectedDate] = useState(new Date().getDate());
 
   const handleClick = (year: number, month: number, date: number) => () => {
@@ -134,9 +104,9 @@ const WeekTiles: React.FC<WeekTilesProps> = (props) => {
           return (
             <Tile
               year={d.getFullYear()}
-              month={d.getMonth()+1}
-              day={weekDays[d.getDay()]} 
-              date={d.getDate()} 
+              month={d.getMonth() + 1}
+              day={weekDays[d.getDay()]}
+              date={d.getDate()}
               onClick={handleClick(d.getFullYear(), d.getMonth(), d.getDate())}
               key={idx}  // unique key
             />
@@ -153,7 +123,7 @@ const WeekTiles: React.FC<WeekTilesProps> = (props) => {
 }
 
 const getLastSunday = (year: number, month: number): Date => {
-  var t = new Date(year, month-1);
+  var t = new Date(year, month - 1);
   t.setDate(t.getDate() - t.getDay());
   return t;
 }
@@ -162,8 +132,8 @@ const afterDate = (date: Date, num: number): Date => new Date(date.getTime() + n
 
 const MonthTiles: React.FC<CalendarPageProps> = (props) => {
   const firstDate = getLastSunday(props.year, props.month)
-  const weeks = Array.from({length: 6}, (_, k) => k).map(week => 
-    Array.from({length: 7}, (_, kk) => kk).map(date => 
+  const weeks = Array.from({ length: 6 }, (_, k) => k).map(week =>
+    Array.from({ length: 7 }, (_, kk) => kk).map(date =>
       afterDate(firstDate, date + 7 * week)
     )
   );
@@ -177,30 +147,88 @@ const MonthTiles: React.FC<CalendarPageProps> = (props) => {
 
   return (
     <Grid container direction="column" alignItems="center" justify="center">
-      {weeks.map((week: Date[], num: number) => 
+      {weeks.map((week: Date[], num: number) =>
         <Grid item>
-          <WeekTiles dates={week} open={selectedWeek[num]} onClick={handleClick(num)}/>
+          <WeekTiles dates={week} open={selectedWeek[num]} onClick={handleClick(num)} />
         </Grid>
-    )}
+      )}
     </Grid>
   )
 }
 
+interface CalendarPageProps {
+  year: number,
+  month: number,
+};
+
+const CalendarPage: React.FC<CalendarPageProps> = (props) => {
+  const classes = useStyles();
+  const [year, setYear] = useState(props.year);
+  const [month, setMonth] = useState(props.month);
+
+  const handleIncrementClick = () => {
+    if (month === 12) {
+      setYear(year + 1);
+      setMonth(1);
+    } else {
+      setMonth(month + 1);
+    }
+  };
+  const handleDecrementClick = () => {
+    if (month === 1) {
+      setYear(year - 1);
+      setMonth(12);
+    } else {
+      setMonth(month - 1);
+    }
+  }
+
+  return (
+    <Grid
+      container
+      direction="column"
+      justify="center"
+      alignItems="stretch"
+    >
+      <Grid item>
+        <Box bgcolor="text.secondary" color="background.paper" p={2} className={classes.month}>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <IconButton onClick={handleDecrementClick}>
+                <ChevronLeftIcon />
+              </IconButton>
+            </Grid>
+            <Grid item xs={6} sm={2}>
+              {year} / {month}
+            </Grid>
+            <Grid item>
+              <IconButton onClick={handleIncrementClick}>
+                <ChevronRightIcon />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Box>
+      </Grid>
+      <Grid item>
+        <MonthTiles year={year} month={month} />
+      </Grid>
+    </Grid>
+  )
+};
+
 export const Table: React.FC = () => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth();
   return (
     <>
       <SearchAppBar />
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="stretch"
-      >
-        <Grid item>
-          <MonthHeader year={2020} month={7} />
-        </Grid>
-        <MonthTiles year={2020} month={7}/>
-      </Grid>
+      <CalendarPage year={year} month={month}/>
     </>
   )
 };
