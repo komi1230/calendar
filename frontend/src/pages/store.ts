57a6e0d59b96e4ba7067d8a3c79e63348777a8cc
komi1230 <yukke.konan@gmail.com>
2020-08-05 18:08:41 +0900

    turned on Redux DevTools

--- frontend/src/pages/store.ts
+++ frontend/src/pages/store.ts
@@ -3,6 +3,7 @@ import { rootReducer } from './rootReducer';
 
 export const store = configureStore({
     reducer: rootReducer,
+    devTools: true,
 });
 
 export type AppDispatch = typeof store.dispatch
