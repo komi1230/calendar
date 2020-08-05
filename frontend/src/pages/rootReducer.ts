f0be75db65cff9c6c2385004d58f91f9b7868093
komi1230 <yukke.konan@gmail.com>
2020-08-05 15:54:48 +0900

    added reducer to rootReducer

--- frontend/src/pages/rootReducer.ts
+++ frontend/src/pages/rootReducer.ts
@@ -1,5 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit';
+import { CalendarModule } from './Calendar/CalendarModule';
 
-export const rootReducer = combineReducers({});
+export const rootReducer = combineReducers({
+    schedules: CalendarModule.reducer,
+});
 
 export type RootState = ReturnType<typeof rootReducer>;
