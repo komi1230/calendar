f92dd0ad7a1dd46eb57c7514aa145a96e1ff9155
komi1230 <yukke.konan@gmail.com>
2020-08-24 21:17:29 +0900

    wrapped with logger

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, middleware, web, App, HttpResponse, HttpServer, Responder};
 
 use calendar::connection::make_pool;
 use calendar::views::{create_user, schedule_content, search_user};
@@ -11,15 +11,21 @@ async fn hello() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=debug");
+    env_logger::init();
+
     let pool = make_pool();
     let app = move || {
-        App::new().service(hello).service(
-            web::scope("/")
-                .data(pool.clone())
-                .service(create_user)
-                .service(search_user)
-                .service(schedule_content),
-        )
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(hello)
+            .service(
+                web::scope("/")
+                    .data(pool.clone())
+                    .service(create_user)
+                    .service(search_user)
+                    .service(schedule_content),
+            )
     };
 
     HttpServer::new(app).bind("127.0.0.1:8000")?.run().await
